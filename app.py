100ea7d54c14bc7865a85e9f8ed6d56b88caa79b
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-11-21 13:18:08 -0300

    update

--- app.py
+++ app.py
@@ -10,23 +10,22 @@ arquivo = 'dados.csv'
 
 def csvCriar():
     if not os.path.exists(arquivo):
-        with open(arquivo, mode="w", newline="") as file:
-            writer = csv.writer(file)
+        with open(arquivo, mode="w") as csvArquivo:
+            writer = csv.writer(csvArquivo)
             writer.writerow(["ID", "Tarefa", "Status"])
 
 def csvEscrever(tarefaList):
-    with open(arquivo, mode="w", newline="") as file:
-        writer = csv.writer(file)
+    with open(arquivo, mode="w") as csvArquivo:
+        writer = csv.writer(csvArquivo)
         writer.writerow(["ID", "Tarefa", "Status"])
         for tarefa in tarefaList:
             writer.writerow([tarefa["ID"], tarefa["Tarefa"], tarefa["Status"]])
 
-
 def csvLer():
-    with open(arquivo, mode="r", newline="") as file:
-        reader = csv.reader(file)
+    with open(arquivo, mode="r") as csvArquivo:
+        reader = csv.reader(csvArquivo)
         next(reader)
-        tarefaList = []
+        listaTarefas = []
         for row in reader:
             if len(row) >= 3:
                 tarefaList.append({
@@ -34,7 +33,7 @@ def csvLer():
                     "Tarefa": row[1],
                     "Status": row[2]
         })
-    return tarefaList
+    return listaTarefas
 
 csvCriar()
 
@@ -47,6 +46,14 @@ def index():
         tarefa for tarefa in tarefas if tarefa["Status"] != "Deletada"
     ]
     return tarefas_visiveis
+
+@app.route("/delete_task/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    for tarefa in listaTarefas:
+        if tarefa["ID"] == task_id:
+            tarefa["Status"] = "Deletada"
+    csvEscrever(listaTarefas)
+    return jsonify({"message": "Tarefa deletada com sucesso!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
